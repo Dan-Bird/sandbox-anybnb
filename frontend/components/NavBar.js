db7f249e31da13f47352dc2f09a780fbe46ea9a7
Daniel <info@daniel-bird.co.uk>
2021-10-20 11:59:22 +0100

    Add a nav

---
+++ frontend/components/NavBar.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/NavBar.module.css';
+
+const NavBar = () => {
+  return (
+    <div className={styles.nav}>
+      <div className={styles.logo}>Anybnb</div>
+    </div>
+  );
+};
+
+export default NavBar;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import NavBar from '@/components/NavBar';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;

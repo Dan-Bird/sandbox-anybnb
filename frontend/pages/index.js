71fd6cc106dba0037560929fb9d001f3f2cd4753
Daniel <info@daniel-bird.co.uk>
2021-10-20 11:49:57 +0100

    Put content in home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,35 @@
-import { sanityClient } from '../sanity';
+import { sanityClient, urlFor } from '../sanity';
+import Link from 'next/link';
+import { Utils } from 'utils';
+import styles from '@/styles/index.module.css';
 
 const index = ({ properties }) => {
-  return <div></div>;
+  return (
+    <>
+      {!!properties && (
+        <div className={styles.main}>
+          <div className={styles.feedContainer}>
+            <h1>Places to Stay Near You</h1>
+            <div className={styles.feed}>
+              {properties.map((property, index) => (
+                <Link href={`property/${property.slug.current}`}>
+                  <div key={property._id} className={styles.card}>
+                    <img src={urlFor(property.mainImage)} />
+                    <p>
+                      {property.reviews.length}{' '}
+                      {Utils.isMultiple('review', property.reviews.length)}
+                    </p>
+                    <h2>{property.title}</h2>
+                    <h3>£{property.pricePerNight}/per night</h3>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export const getServerSideProps = async () => {
